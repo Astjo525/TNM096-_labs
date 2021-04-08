cf5fc7d1ebb31d0894221a85197bba6dddc397d3
astjo525 <astrid.c.johansson@gmail.com>
2021-04-08 18:55:25 +0200

    Lab1: some more optimization

--- Lab1/main.py
+++ Lab1/main.py
@@ -29,8 +29,9 @@ class State:
         self.evaluation = self.cost + self.h1
  
     def mat_to_string(self, matrix):
-        # matrix = matrix.reshape((1, 9))
-        # return "".join(map(str, matrix))
+        # ÄNDRAT: Blev bättre
+        #matrix = matrix.reshape((1, 9))
+        #return "".join(map(str, matrix))
         return np.array2string(matrix)
 
 
@@ -39,8 +40,10 @@ class PuzzleSolver:
         self.current_state = start_state
         self.goal_state = goal_state
         self.counter = 0
-        self.open = [(self.current_state.evaluation, self.counter, self.current_state)]
-        hq.heapify(self.open)
+        self.open = []
+        hq.heappush(self.open, (self.current_state.evaluation, self.counter, self.current_state)) # ÄNDRAD: blev lite bättre
+        #self.open = [(self.current_state.evaluation, self.counter, self.current_state)]
+        #hq.heapify(self.open)
         self.close = {}
 
     def heuristic_search(self):
@@ -49,7 +52,7 @@ class PuzzleSolver:
         moves = 0
 
         while(self.open):
-            priority, counter, self.current_state = hq.heappop(self.open)
+            _, _ , self.current_state = hq.heappop(self.open)
             #self.close.update({matrix_to_string(self.current_state.get_state_mat())})
             #hq.heappush(self.close, (self.current_state.string_mat))
             self.close[self.current_state.string_mat] = 1
@@ -65,7 +68,10 @@ class PuzzleSolver:
     def swap(self, curr_pos, adv_pos):
         adv_state = self.current_state.state_mat.copy()
 
-        adv_state[curr_pos[0], curr_pos[1]], adv_state[adv_pos[0], adv_pos[1]] = adv_state[adv_pos[0], adv_pos[1]], adv_state[curr_pos[0], curr_pos[1]]
+        #adv_state[curr_pos[0], curr_pos[1]], adv_state[adv_pos[0], adv_pos[1]] = adv_state[adv_pos[0], adv_pos[1]], adv_state[curr_pos[0], curr_pos[1]]
+
+        adv_state[curr_pos[0], curr_pos[1]] = adv_state[adv_pos[0], adv_pos[1]] # ÄNDRAT: blev typ lite snabbare
+        adv_state[adv_pos[0], adv_pos[1]] = 0 
 
         return adv_state
     
@@ -75,6 +81,9 @@ class PuzzleSolver:
             return True
 
         return False
+
+
+        
         
     def find_possible_movements(self):
         posy, posx = np.where(self.current_state.state_mat == 0)
@@ -125,10 +134,10 @@ if __name__ == '__main__':
     #start_state = np.array([[4, 1, 3], [7, 2, 6], [0, 5, 8]])
 
     # MEDIUM
-    start_state = np.array([[7, 2, 4], [5, 0, 6], [8, 3, 1]])
+    #start_state = np.array([[7, 2, 4], [5, 0, 6], [8, 3, 1]])
 
     # DIFFICULT
-    #start_state = np.array([[6, 4, 7], [8, 5, 0], [3, 2, 1]])
+    start_state = np.array([[6, 4, 7], [8, 5, 0], [3, 2, 1]])
 
 
     goal_state = goal_state.reshape(3,3)
